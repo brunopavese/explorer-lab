e6a44ffc68be69029f7dcd8c1fe3763fcebcecc7
Bruno Pavese <pavese.bruno@gmail.com>
2022-10-23 18:35:54 -0300

    feature: função setCard (visa, masterard, elo)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCard(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#FF5F00", "#F79E1B"],
+    elo: ["#00A4E0", "#EF4123"],
+    default: ["black", "grey"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCard = setCard
